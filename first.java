9db6755c534c632b87d677b1de04283a2eb984a4
gsharshith <133838136+gsharshith@users.noreply.github.com>
2023-05-17 09:30:50 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,13 @@
+package training;
+public class first {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; // Example array
+
+        System.out.println("Elements of the array:");
+
+        // Loop through each element in the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
